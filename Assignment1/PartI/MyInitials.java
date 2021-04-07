369c83af66256513fdeafffbd4e2469ce654c5d5
ah2020-cs <81828176+ah2020-cs@users.noreply.github.com>
2021-04-06 23:29:18 -0400

    Add files via upload

---
+++ Assignment1/PartI/MyInitials.java
@@ -0,0 +1,17 @@
+
+public class MyInitials {
+
+	public static void main(String[] args) {
+		
+		System.out.println();
+		System.out.println("  A    M   M  H   H");
+		System.out.println(" A A   MM MM  H   H");
+		System.out.println("A   A  MM MM  H   H");
+		System.out.println("AAAAA  M M M  HHHHH");
+		System.out.println("A   A  M   M  H   H");
+		System.out.println("A   A  M   M  H   H");
+		System.out.println("A   A  M   M  H   H");
+		System.out.println();
+
+	}
+}
